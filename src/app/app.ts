9b79511bc641852081ab4ffef72a209eb068b20e
Michael Small <michael.small@relationshipone.com>
2026-08-21 10:04:36 -0500

    chore: remove stock root component HTML/CSS

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet],
   selector: 'app-root',
-  styleUrl: './app.css',
-  templateUrl: './app.html',
+  template: ` <h1>Angular AI Q3 2026</h1> `,
 })
-export class App {
-  protected readonly title = signal('angular-ai-Q3-2026');
-}
+export class App {}
