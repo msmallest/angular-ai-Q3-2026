0ffd501fc59273c9df769e7b8d5b9b394152604e
Michael Small <michael.small@relationshipone.com>
2026-08-21 13:06:44 -0500

    chore: point to `README.md` at app root

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,12 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  template: ` <h1>Angular AI Q3 2026</h1> `,
+  template: `
+    <h1>Angular AI Q3 2026</h1>
+    <p>
+      Reference <code>README.md</code> for info on Angular's integrations with Skills and info about
+      the Angular CLI MCP Server.
+    </p>
+  `,
 })
 export class App {}
